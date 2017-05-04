6cc357a57e51080c27d7f25809c0cd132a8fe19d
Tommy Durant <thomas.durant@yale.edu>
2017-05-03 22:09:39 -0400

    changed spaces to tabs

--- 2-ingestion/python-cli/kafka_to_hdfs.py
+++ 2-ingestion/python-cli/kafka_to_hdfs.py
@@ -26,27 +26,27 @@ for msg in kafka_client:
 	i = i + 1
 
 	queue.append(msg.value)
-    
-    for seg in segments:
-        fields = seg.split('|')
-        if fields[0] == 'MSH':
-            hl7['msg_ts'] = fields[6]
-        if fields[0] == 'OBX':
-            hl7['component'] = fields[3]
-            hl7['value'] = fields[5]
-            hl7['unit'] = fields[6]
-            hl7['result_ts'] = fields[14]
-            hl7['method'] = fields[18]
-        if fields[0] == 'OBR':
-            hl7['order_ts'] = fields[6]
-            hl7['coll_ts'] = fields[8]
+
+	for seg in segments:
+		fields = seg.split('|')
+		if fields[0] == 'MSH':
+			hl7['msg_ts'] = fields[6]
+		if fields[0] == 'OBX':
+			hl7['component'] = fields[3]
+			hl7['value'] = fields[5]
+			hl7['unit'] = fields[6]
+			hl7['result_ts'] = fields[14]
+			hl7['method'] = fields[18]
+		if fields[0] == 'OBR':
+			hl7['order_ts'] = fields[6]
+			hl7['coll_ts'] = fields[8]
 	
 	data['messages'].append(hl7)
-    keys = hl7.keys()
-    with open('test.csv', 'wb') as output_file:
-        dict_writer = csv.DictWriter(output_file, keys)
-        dict_writer.writeheader()
-        dict_writer.writerows(data['messages'])
+	keys = hl7.keys()
+	with open('test.csv', 'wb') as output_file:
+		dict_writer = csv.DictWriter(output_file, keys)
+		dict_writer.writeheader()
+		dict_writer.writerows(data['messages'])
 
 	if i % 100 == 0:
 		with hdfs_client.write("/tmp/test_" + str(file) + ".hl7") as writer:
